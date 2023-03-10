7935abbbeabd3034691d13b7c8adc75c2efb90fd
msh2481 <msh2481@mail.ru>
2023-03-10 19:51:58 +0100

    Row linear add

--- microbusiness_density_forecasing/main.py
+++ microbusiness_density_forecasing/main.py
@@ -1,7 +1,16 @@
+import numpy as np
 import pandas as pd
 import torch
+from matplotlib import pyplot as plt
 
-from solve import *
+from solve import (
+    last_density,
+    last_density_corrected,
+    row_linear,
+    row_linear_extra,
+    row_linear_extra_log,
+    row_linear_add,
+)
 
 """
 TODO:
@@ -29,7 +38,49 @@ def submission(data, name):
     df.to_csv(name, index=False)
 
 
+def smape(a: np.ndarray, b: np.ndarray) -> float:
+    eps = 1e-9
+    return (200 * np.abs(a - b) / (np.abs(a) + np.abs(b) + eps)).mean()
+
+
 X_test = torch.load("X_test.p")
 assert X_test.shape == (N_SERIES, T_PREDICT, N_FEATURES)
-y_test = row_linear_extra(X_train, y_train, X_test)
-submission(y_test, "submissions/row_linear_extra.csv")
+
+
+def validate(
+    solve, train_l: int, train_r: int, val_l: int, val_r: int, visualize: bool = False
+):
+    assert train_l < train_r <= val_l < val_r
+
+    p = solve(
+        X_train[:, train_l:train_r, :],
+        y_train[:, train_l:train_r],
+        X_train[:, val_l:val_r, :],
+    )
+    y = y_train[:, val_l:val_r]
+    if visualize:
+        eps = 1e-9
+        delta = np.log(p + eps) - np.log(y + eps)
+        print("100 log-MAE:", 100 * np.abs(delta).mean())
+        samples = 256
+        print(f"Sampling {samples} out of {delta.shape[0]} predictions")
+        idx = np.random.choice(delta.shape[0], size=samples, replace=False)
+        plt.subplot(1, 2, 1)
+        for i in idx:
+            plt.plot(delta[i], "-ob", ms=0.5, lw=0.5, alpha=0.2)
+        plt.ylim((-0.2, 0.2))
+        plt.grid("major")
+        plt.subplot(1, 2, 2)
+        plt.plot(100 * np.abs(delta).mean(axis=0), "-ob")
+        plt.ylim((0, 10))
+        plt.grid("major")
+        plt.show()
+    return smape(p, y)
+
+
+val_len, skip, r = 3, 1, T_AVAILABLE
+result = validate(row_linear_add, 0, r - val_len - skip, r - val_len, r, visualize=True)
+print("SMAPE:", result)
+
+y_test = row_linear_add(X_train, y_train, X_test)
+submission(y_test, "submissions/row_linear_add.csv")
--- microbusiness_density_forecasing/solve.py
+++ microbusiness_density_forecasing/solve.py
@@ -3,6 +3,7 @@ import torch
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import Lasso
+from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler
 from tqdm import tqdm
@@ -90,10 +91,12 @@ class LogFeatures:
     """
 
     def fit(self, X, y=None):
-        pass
+        return self
 
     def transform(self, X):
-        return torch.cat((X, X.log()), axis=-1)
+        eps = 1e-9
+        L = np.log(np.abs(X) + eps)
+        return np.concatenate((X, L), axis=-1)
 
 
 def row_linear(
@@ -173,7 +176,7 @@ def row_linear_extra(
     return y_test
 
 
-def row_linear_log(
+def row_linear_extra_log(
     X_train: torch.Tensor, y_train: torch.Tensor, X_test: torch.Tensor
 ) -> torch.Tensor:
     N_SERIES, N_FEATURES = X_train.size(0), X_train.size(2)
@@ -182,16 +185,131 @@ def row_linear_log(
     assert y_train.shape == (N_SERIES, T_AVAILABLE)
     assert X_test.shape == (N_SERIES, T_PREDICT, N_FEATURES)
 
-    pipeline = Pipeline([("log", LogFeatures()), ("scale", RobustScaler())])
+    pipeline = Pipeline(
+        [
+            ("A", Reshaper([-1, N_FEATURES])),
+            ("B", RobustScaler()),
+            ("C", LogFeatures()),
+            ("D", Reshaper([N_SERIES, -1, 2 * N_FEATURES])),
+        ]
+    )
 
     X_train = pipeline.fit_transform(X_train)
     X_test = pipeline.transform(X_test)
-    y_train = y_train.log()
+    y_train = y_train.clamp_min(min=1).log().numpy()
+    y_shifted = np.roll(y_train, shift=1, axis=1)
+    X_train = np.concatenate((X_train, y_shifted[:, :, np.newaxis]), axis=-1)
+    estimator = Lasso(alpha=0.001).fit(
+        X_train.reshape(-1, 2 * N_FEATURES + 1), y_train.flatten()
+    )
+
+    print(estimator.coef_)
 
     y_test = torch.empty((N_SERIES, T_PREDICT))
     for i in range(N_SERIES):
         for j in range(T_PREDICT):
-            y_test[i, j] = y_train[i, -1] + (X_train[i, -1, 0] / X_test[i, j, 0]).log()
+            prev = y_train[i, -1]  # or make auto-regressive
+            row = np.concatenate(
+                (X_test[i, j].reshape(1, -1), prev.reshape(1, 1)), axis=-1
+            )
+            p = estimator.predict(row).item()
+            y_test[i, j] = p
+
+    y_test = y_test.exp()
+    return y_test
+
+
+def row_linear_add(
+    X_train: torch.Tensor, y_train: torch.Tensor, X_test: torch.Tensor
+) -> torch.Tensor:
+    N_SERIES, N_FEATURES = X_train.size(0), X_train.size(2)
+    T_AVAILABLE, T_PREDICT = X_train.size(1), X_test.size(1)
+    assert X_train.shape == (N_SERIES, T_AVAILABLE, N_FEATURES)
+    assert y_train.shape == (N_SERIES, T_AVAILABLE)
+    assert X_test.shape == (N_SERIES, T_PREDICT, N_FEATURES)
+
+    pipeline = Pipeline(
+        [
+            ("A", Reshaper([-1, N_FEATURES])),
+            ("B", RobustScaler()),
+            ("C", LogFeatures()),
+            ("D", Reshaper([N_SERIES, -1, 2 * N_FEATURES])),
+        ]
+    )
+
+    X_train = pipeline.fit_transform(X_train)
+    X_test = pipeline.transform(X_test)
+    y_train = y_train.clamp_min(min=1).log().numpy()
+    y_shifted = np.roll(y_train, shift=1, axis=1)
+    y_delta = y_train - y_shifted
+
+    estimator = Lasso(alpha=1e-9).fit(
+        X_train.reshape(-1, 2 * N_FEATURES), y_delta.flatten()
+    )
+    print(estimator.coef_)
+
+    y_test = torch.empty((N_SERIES, T_PREDICT))
+    for i in range(N_SERIES):
+        for j in range(T_PREDICT):
+            prev = y_train[i, -1] if j == 0 else y_test[i, j - 1]
+            row = X_test[i, j].reshape(1, -1)
+            p = (prev + estimator.predict(row)).item()
+            y_test[i, j] = p
+
+    y_test = y_test.exp()
+    return y_test
+
+
+def row_mlp_extra(
+    X_train: torch.Tensor, y_train: torch.Tensor, X_test: torch.Tensor
+) -> torch.Tensor:
+    N_SERIES, N_FEATURES = X_train.size(0), X_train.size(2)
+    T_AVAILABLE, T_PREDICT = X_train.size(1), X_test.size(1)
+    assert X_train.shape == (N_SERIES, T_AVAILABLE, N_FEATURES)
+    assert y_train.shape == (N_SERIES, T_AVAILABLE)
+    assert X_test.shape == (N_SERIES, T_PREDICT, N_FEATURES)
+
+    pop_train = X_train[:, :, 0].copy()
+    pop_test = X_test[:, :, 0].copy()
+
+    pipeline = Pipeline(
+        [
+            ("A", Reshaper([-1, N_FEATURES])),
+            ("B", RobustScaler()),
+            ("C", LogFeatures()),
+            ("D", Reshaper([N_SERIES, -1, 2 * N_FEATURES])),
+        ]
+    )
+
+    X_train = pipeline.fit_transform(X_train)
+    X_test = pipeline.transform(X_test)
+    y_train = y_train.clamp_min(min=1).log().numpy()
+    y_shifted = np.roll(y_train, shift=1, axis=1)
+    y_delta = y_train - y_shifted
+
+    # estimator = MLPRegressor(
+    #     hidden_layer_sizes=(10,),
+    #     activation="relu",
+    #     solver="adam",
+    #     alpha=0.01,
+    # ).fit(X_train.reshape(-1, 2 * N_FEATURES), y_delta.flatten())
+
+    estimator = Lasso(alpha=1e-9).fit(
+        X_train.reshape(-1, 2 * N_FEATURES), y_delta.flatten()
+    )
+    print(estimator.coef_)
+
+    y_test = torch.empty((N_SERIES, T_PREDICT))
+    for i in range(N_SERIES):
+        for j in range(T_PREDICT):
+            prev = (
+                y_train[i, -1] * (pop_train[i, -1] / pop_test[i, 0])
+                if j == 0
+                else y_test[i, j - 1]
+            )
+            row = X_test[i, j].reshape(1, -1)
+            p = (prev + estimator.predict(row)).item()
+            y_test[i, j] = p
 
     y_test = y_test.exp()
     return y_test
